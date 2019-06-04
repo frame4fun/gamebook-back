7e205b475d96dc90530ad8aa9e5bfebd461a0a07
guillaume <guillaume.rahbari@gmail.com>
2019-06-04 22:34:32 +0200

    improve docker

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
+import mongoose from 'mongoose';
 import { createServer } from 'http';
 import app from './app';
 import dbConnect from './models/connection';
+
+const server = createServer(app);
 
 dbConnect(() => {
   // eslint-disable-next-line no-console
@@ -8,13 +11,33 @@ dbConnect(() => {
 
   app.set('port', normalizePort(process.env.PORT || 8080));
 
-  const server = createServer(app);
   server.listen(app.get('port'), () => {
     // eslint-disable-next-line no-console
     console.log(
       '✔ Server listening on port'.green,
       String(app.get('port')).cyan
     );
+    process.send('ready');
+  });
+});
+
+process.on('SIGINT', () => {
+  console.info('SIGINT signal received.');
+
+  // Stops the server from accepting new connections and finishes existing connections.
+  server.close(function(err) {
+    // if error, log and exit with error (1 code)
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    // close your database connection and exit with success (0 code)
+    // for example with mongoose
+    mongoose.connection.close(function() {
+      console.log('Mongoose connection disconnected');
+      process.exit(0);
+    });
   });
 });
 
